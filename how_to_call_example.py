08c58bfdd4a29649b63fadc323b3db0d645e3063
skibert280 <bertski280@gmail.com>
2025-03-26 23:28:12 +0100

    Add files via upload

---
+++ how_to_call_example.py
@@ -0,0 +1,20 @@
+import sys
+import time
+
+from licensifylib import verify_and_activate_license, 
+from licensifylib import create_license_file 
+
+print(f"PROGRAM STARTS")
+
+
+# 1st parameter: "TEST_COMMON" or "TEST_PERSONAL" or "PRO"
+
+# CREATE LICENSE:
+create_license_file("TEST_COMMON", "Andy Munjaro", "YourApplication", "licensify_test_lic.bin", expiry_days=1)
+time.sleep(5) 
+
+# LICENSE CHECK:
+if not verify_and_activate_license("YourApplication", "licensify_test_lic.bin"): sys.exit(0)
+
+
+print(f"PROGRAM ENDS")
